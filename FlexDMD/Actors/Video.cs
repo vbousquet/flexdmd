a1af00c74702d6dabcbecd08d531f080f6e34824
Vincent Bousquet <vcbousquet@gmail.com>
2019-12-18 19:46:06 +0100

    Switch to NAudio, MFNet nuget packages, StrongNaming, clean up

--- FlexDMD/Actors/Video.cs
+++ FlexDMD/Actors/Video.cs
@@ -14,23 +14,15 @@
    */
 using FlexDMD.Actors;
 using MediaFoundation;
+using MediaFoundation.Interop;
 using MediaFoundation.Misc;
 using MediaFoundation.ReadWrite;
+using NAudio.Wave;
 using NLog;
 using System;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
 
-// Based on : https://docs.microsoft.com/en-us/windows/win32/medfound/processing-media-data-with-the-source-reader?redirectedfrom=MSDN
-// Using : https://docs.microsoft.com/en-us/windows/win32/medfound/uncompressed-video-buffers
-//
-// FIXME add sound decoding support (see https://github.com/microsoft/Windows-classic-samples/blob/master/Samples/Win7Samples/multimedia/mediafoundation/AudioClip/main.cpp)
-// http://csharphelper.com/blog/2016/08/play-an-audio-resource-in-c/
-// https://docs.microsoft.com/en-us/dotnet/api/system.media.soundplayer?view=netframework-4.8
-// Wevout : https://www.codeproject.com/Articles/866347/Streaming-Audio-to-the-WaveOut-Device
-// https://www.codeproject.com/Articles/3352/A-low-level-audio-player-in-C
-// https://stackoverflow.com/questions/32975300/low-level-audio-player-with-c-sharp-playback-stutter-fix
 namespace FlexDMD
 {
     class Video : AnimatedActor
@@ -40,10 +32,10 @@ namespace FlexDMD
         private readonly string _path;
         private readonly int _stretchMode; // stretch: 0, crop to top: 1, crop to center: 2, crop to bottom: 3
         private int _videoWidth, _videoHeight;
-        private IMFSourceReader _reader;
+        private IMFSourceReader _videoReader;
+        private MediaFoundationReader _audioReader;
+        private WaveOutEvent _audioDevice;
         private Bitmap _frame;
-
-        static woLib WaveOut = new woLib(); // interface to classic windows wave-out device
 
         // FIXME initialize/dispose when entering/exiting rendering graph (MMF has limited number of concurrently opened sources)
 
@@ -56,6 +48,7 @@ namespace FlexDMD
 
         public void Open()
         {
+            _audioDevice = new WaveOutEvent();
             Rewind();
             nOpenedVideos++;
             log.Info("Video opened: {0} ({1} videos concurrently opened)", _path, nOpenedVideos);
@@ -63,96 +56,42 @@ namespace FlexDMD
 
         public void Close()
         {
+            _audioDevice.Dispose();
+            _audioDevice = null;
             nOpenedVideos--;
-            if (_reader != null) SafeRelease(ref _reader);
-            WaveOut.ResetWODevice();
+            if (_videoReader != null) ComClass.SafeRelease(ref _videoReader);
+            // WaveOut.ResetWODevice();
         }
 
         protected override void Rewind()
         {
             // log.Info("Initalizing video: {0}", _path);
-            if (_reader != null) SafeRelease(ref _reader);
-            HResult hr = MFExtern.MFCreateSourceReaderFromURL(_path, null, out IMFSourceReader reader);
-            if (MFError.Failed(hr))
+            if (_videoReader != null)
             {
-                log.Error("Failed to open video: {0}", _path);
-                return;
+                // should pass in a variant of type VT_I8 which is a long containing time in 100nanosecond units
+                var pv = new PropVariant(0L);
+                _videoReader.SetCurrentPosition(Guid.Empty, pv);
             }
-            processAudio(reader);
-            _reader = SetupVideoDecoder(reader);
-            ReadNextFrame();
-        }
-
-        private void processAudio(IMFSourceReader reader)
-        {
-            // HResult hr = MFExtern.MFCreateSourceReaderFromURL(_path, null, out IMFSourceReader reader);
-            // if (MFError.Succeeded(hr))
-            HResult hr = SetupAudioDecoder(reader);
-            if (MFError.Failed(hr))
-                return;
-            IMFMediaBuffer buffer = null;
-            IMFSample sample = null;
-            while (true)
+            else
             {
-                HResult result = reader.ReadSample((int)MF_SOURCE_READER.FirstAudioStream, 0, out int streamIndex, out MF_SOURCE_READER_FLAG flags, out long timeStamp, out sample);
-                if (MFError.Failed(hr))
-                    break;
-                if (flags == MF_SOURCE_READER_FLAG.CurrentMediaTypeChanged)
-                    log.Error("Current type changed [unsupported]");
-                if (flags == MF_SOURCE_READER_FLAG.EndOfStream)
+                HResult hr = MFExtern.MFCreateSourceReaderFromURL(_path, null, out IMFSourceReader reader);
+                if (MFError.Succeeded(hr))
                 {
-                    log.Info("End of stream");
-                    break;
+                    _videoReader = SetupVideoDecoder(reader);
+                    log.Error("Failed to open video: {0}", _path);
                 }
-                if (sample == null)
-                    continue;
-                hr = sample.ConvertToContiguousBuffer(out buffer);
-                if (MFError.Failed(hr))
-                    break;
-                hr = buffer.Lock(out IntPtr ppbBuffer, out int pcbMaxLength, out int pcbCurrentLength);
-                if (MFError.Failed(hr))
-                    break;
-                // TODO enqueue for playing in waveout
-                WaveOut.SendWODevice(ppbBuffer, (uint)pcbCurrentLength);
-                hr = buffer.Unlock();
-                if (MFError.Failed(hr))
-                    break;
-                SafeRelease(ref buffer);
-                SafeRelease(ref sample);
             }
-            SafeRelease(ref buffer);
-            SafeRelease(ref sample);
-            // SafeRelease(ref reader);
-        }
-
-        private HResult SetupAudioDecoder(IMFSourceReader reader)
-        {
-            // Setup decoder to uncompressed PCM
-            HResult hr = MFExtern.MFCreateMediaType(out IMFMediaType outputType);
-            if (MFError.Succeeded(hr))
-                hr = outputType.SetGUID(MFAttributesClsid.MF_MT_MAJOR_TYPE, MFMediaType.Audio);
-            if (MFError.Succeeded(hr))
-                hr = outputType.SetGUID(MFAttributesClsid.MF_MT_SUBTYPE, MFMediaType.PCM);
-            if (MFError.Succeeded(hr))
-                hr = reader.SetCurrentMediaType((int)MF_SOURCE_READER.FirstAudioStream, null, outputType);
-            if (MFError.Succeeded(hr))
-                hr = reader.SetStreamSelection((int)MF_SOURCE_READER.FirstAudioStream, true);
-            SafeRelease(ref outputType);
-
-            // Setup waveout with the right output format
-            IMFMediaType currentType = null;
-            if (MFError.Succeeded(hr))
-                hr = reader.GetCurrentMediaType((int)MF_SOURCE_READER.FirstAudioStream, out currentType);
-            int cChannels = 0, samplesPerSec = 0, bitsPerSample = 0;
-            if (MFError.Succeeded(hr))
-                hr = currentType.GetUINT32(MFAttributesClsid.MF_MT_AUDIO_NUM_CHANNELS, out cChannels);
-            if (MFError.Succeeded(hr))
-                hr = currentType.GetUINT32(MFAttributesClsid.MF_MT_AUDIO_SAMPLES_PER_SECOND, out samplesPerSec);
-            if (MFError.Succeeded(hr))
-                hr = currentType.GetUINT32(MFAttributesClsid.MF_MT_AUDIO_BITS_PER_SAMPLE, out bitsPerSample);
-            SafeRelease(ref currentType);
-            WaveOut.InitWODevice((uint)samplesPerSec, (uint)cChannels, (uint)bitsPerSample, false);
-            return hr;
+            if (_audioReader != null)
+            {
+                _audioReader.Seek(0, System.IO.SeekOrigin.Begin);
+            }
+            else
+            {
+                _audioReader = new MediaFoundationReader(_path);
+                _audioDevice.Init(_audioReader);
+                _audioDevice.Play();
+            }
+            ReadNextFrame();
         }
 
         private IMFSourceReader SetupVideoDecoder(IMFSourceReader reader)
@@ -161,7 +100,7 @@ namespace FlexDMD
             HResult hr = reader.GetNativeMediaType((int)MF_SOURCE_READER.FirstVideoStream, 0, out IMFMediaType nativeType);
             if (MFError.Succeeded(hr))
                 hr = MFExtern.MFGetAttributeSize(nativeType, MFAttributesClsid.MF_MT_FRAME_SIZE, out _videoWidth, out _videoHeight);
-            SafeRelease(ref nativeType);
+            ComClass.SafeRelease(ref nativeType);
 
             // Setup decoder to RGB24
             IMFMediaType outputType = null;
@@ -175,26 +114,26 @@ namespace FlexDMD
                 hr = reader.SetCurrentMediaType((int)MF_SOURCE_READER.FirstVideoStream, null, outputType);
             if (MFError.Succeeded(hr))
                 hr = reader.SetStreamSelection((int)MF_SOURCE_READER.FirstVideoStream, true);
-            SafeRelease(ref outputType);
+            ComClass.SafeRelease(ref outputType);
             if (MFError.Failed(hr))
             {
                 log.Error("Failed to initialize video: {0}", _path);
-                SafeRelease(ref reader);
+                ComClass.SafeRelease(ref reader);
             }
             return reader;
         }
 
         protected override void ReadNextFrame()
         {
-            if (_reader == null) Rewind();
-            if (_reader == null || _endOfAnimation) return;
+            if (_videoReader == null) Rewind();
+            if (_videoReader == null || _endOfAnimation) return;
             try
             {
-                HResult result = _reader.ReadSample((int)MF_SOURCE_READER.FirstVideoStream, 0, out int streamIndex, out MF_SOURCE_READER_FLAG flags, out long timeStamp, out IMFSample sample);
+                HResult result = _videoReader.ReadSample((int)MF_SOURCE_READER.FirstVideoStream, 0, out int streamIndex, out MF_SOURCE_READER_FLAG flags, out long timeStamp, out IMFSample sample);
                 if (result < 0)
                 {
                     log.Info("Failed to read sample from {0} {1}", _path, result);
-                    SafeRelease(ref sample);
+                    ComClass.SafeRelease(ref sample);
                     return;
                 }
                 if (flags == MF_SOURCE_READER_FLAG.EndOfStream)
@@ -210,7 +149,7 @@ namespace FlexDMD
                 {
                     log.Info("Native type changed");
                     // The format changed. Reconfigure the decoder.
-                    _reader = SetupVideoDecoder(_reader);
+                    _videoReader = SetupVideoDecoder(_videoReader);
                 }
                 if (flags == MF_SOURCE_READER_FLAG.CurrentMediaTypeChanged)
                 {
@@ -241,7 +180,7 @@ namespace FlexDMD
                     }
                     _frameTime = frameTime / 10000000.0f;
                     _frameDuration = frameDuration / 10000000.0f;
-                    SafeRelease(ref sample);
+                    ComClass.SafeRelease(ref sample);
                 }
             }
             catch (Exception e)
@@ -260,22 +199,6 @@ namespace FlexDMD
         public override string ToString()
         {
             return string.Format("Video '{0}'", _path);
-        }
-
-        /// <summary>
-        /// Realease a COM instance and set the variable to null.
-        /// </summary>
-        /// <typeparam name="T">A COM Interface.</typeparam>
-        /// <param name="obj">A COM instance to release and then set to null.</param>
-        public static void SafeRelease<T>(ref T obj) where T : class
-        {
-            if (obj == null) return;
-
-            if (!Marshal.IsComObject(obj))
-                throw new ArgumentException("obj is not a COM object.", "obj");
-
-            Marshal.ReleaseComObject(obj);
-            obj = null;
         }
     }
 
