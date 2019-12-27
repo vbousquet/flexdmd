633fcdfdae0eda8f5687951faaace2b399390214
Vincent Bousquet <vcbousquet@gmail.com>
2019-12-27 23:35:13 +0100

    Fix image read fail in VPX table

--- FlexDMD/VPXFile.cs
+++ FlexDMD/VPXFile.cs
@@ -27,12 +27,19 @@ namespace FlexDMD
 {
     public class VPXFile : IDisposable
     {
-        private readonly Dictionary<string, CFStream> _images = new Dictionary<string, CFStream>();
+        private readonly Dictionary<string, Entry> _images = new Dictionary<string, Entry>();
         private CompoundFile _cf;
+
+        private class Entry
+        {
+            public string _file;
+            public string _name;
+            public CFStream _stream;
+        }
 
         public VPXFile(string filename)
         {
-            _cf = new CompoundFile(filename);
+            _cf = new CompoundFile(filename, CFSUpdateMode.ReadOnly, CFSConfiguration.Default);
             CFStorage gameStg = _cf.RootStorage.GetStorage("GameStg");
             UInt32 version = new VPXReader(gameStg.GetStream("Version")).ReadUInt32();
             void visitor(CFItem item)
@@ -53,12 +60,17 @@ namespace FlexDMD
             return _images.ContainsKey(name);
         }
 
+        public string GetImportFile(string name)
+        {
+            if (_images.TryGetValue(name, out Entry entry)) return entry._file;
+            return null;
+        }
+
         public Stream OpenStream(string name)
         {
-            CFStream stream;
-            if (_images.TryGetValue(name, out stream))
+            if (_images.TryGetValue(name, out Entry entry))
             {
-                var data = ReadImage(stream, false);
+                var data = ReadImage(entry._stream, false);
                 return new MemoryStream(data);
             }
             return null;
@@ -71,6 +83,8 @@ namespace FlexDMD
             int size = 0;
             var done = false;
             byte[] data = null;
+            var entry = new Entry();
+            entry._stream = stream;
             while (!done)
             {
                 var bytesInRecordRemaining = reader.ReadUInt32();
@@ -84,12 +98,11 @@ namespace FlexDMD
                             done = true;
                         break;
                     case "NAME":
-                        var name = reader.ReadLenPrefixedString(Encoding.ASCII);
-                        _images[name] = stream;
-                        if (nameOnly) done = true;
+                        entry._name = reader.ReadLenPrefixedString(Encoding.ASCII);
+                        _images[entry._name] = entry;
                         break;
                     case "PATH":
-                        reader.ReadLenPrefixedString(Encoding.ASCII);
+                        entry._file = reader.ReadLenPrefixedString(Encoding.ASCII);
                         break;
                     case "INME":
                         reader.ReadLenPrefixedString(Encoding.ASCII);
@@ -110,23 +123,28 @@ namespace FlexDMD
                         inJpg = true;
                         break;
                     case "DATA":
-                        data = reader.Read(size);
-                        if (data[0] == 0x89 && data[1] == 0x50 && data[2] == 0x4E && data[3] == 0x47 && data[4] == 0x0D && data[5] == 0x0A && data[6] == 0x1A && data[7] == 0x0A)
-                        {
-                            // PNG
-                        }
-                        else if (data[0] == 0xFF && data[1] == 0xD8)
-                        {
-                            // JPG
-                        }
-                        else if (data[0] == 0x23 && data[1] == 0x3F && data[2] == 0x52 && data[3] == 0x41 && data[4] == 0x44 && data[5] == 0x49 && data[6] == 0x41 && data[7] == 0x4E && data[8] == 0x43 && data[9] == 0x45 && data[10] == 0x0A)
-                        {
-                            // HDR (unsupported)
-                            data = null;
-                        }
+                        if (nameOnly) 
+                            done = true;
                         else
                         {
-                            data = null;
+                            data = reader.Read(size);
+                            if (data[0] == 0x89 && data[1] == 0x50 && data[2] == 0x4E && data[3] == 0x47 && data[4] == 0x0D && data[5] == 0x0A && data[6] == 0x1A && data[7] == 0x0A)
+                            {
+                                // PNG
+                            }
+                            else if (data[0] == 0xFF && data[1] == 0xD8)
+                            {
+                                // JPG
+                            }
+                            else if (data[0] == 0x23 && data[1] == 0x3F && data[2] == 0x52 && data[3] == 0x41 && data[4] == 0x44 && data[5] == 0x49 && data[6] == 0x41 && data[7] == 0x4E && data[8] == 0x43 && data[9] == 0x45 && data[10] == 0x0A)
+                            {
+                                // HDR (unsupported)
+                                data = null;
+                            }
+                            else
+                            {
+                                data = null;
+                            }
                         }
                         break;
                 }
@@ -145,7 +163,7 @@ namespace FlexDMD
             {
                 _pos = 0;
                 _stream = stream;
-                _reverse = BitConverter.IsLittleEndian;
+                _reverse = !BitConverter.IsLittleEndian;
             }
 
             public void Skip(int count)
@@ -188,6 +206,7 @@ namespace FlexDMD
             {
                 var buffer = new byte[length];
                 _stream.Read(buffer, _pos, length);
+                _pos += length;
                 return buffer;
             }
         }
