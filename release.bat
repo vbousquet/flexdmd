ef3882e6ddef3b930b7791bacfb327a3ab1e7691
Vincent Bousquet <vcbousquet@gmail.com>
2020-02-03 22:22:43 +0100

    Simpler release batch script

--- release.bat
+++ release.bat
@@ -14,6 +14,11 @@ REM Build
 
 REM Package
 mkdir Build
+del Build\FlexDMDUI.exe
+del Build\FlexDMD.zip
+del Build\FlexDMD.dll
+del Build\FlexUDMD.dll
 copy FlexDMDUI\bin\Release\FlexDMDUI.exe Build\FlexDMDUI.exe
 copy FlexDMD\bin\Release\ILMerge\FlexDMD.dll Build\FlexDMD.dll
 copy FlexUDMD\bin\Release\FlexUDMD.dll Build\FlexUDMD.dll
+powershell.exe -nologo -noprofile -command "Compress-Archive -CompressionLevel Optimal -Path Build\FlexDMD*.* -DestinationPath Build\FlexDMD.zip"
