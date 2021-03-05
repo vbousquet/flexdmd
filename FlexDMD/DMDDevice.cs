b9fe50ac4821c2ca90775bde1979361d917f179f
Vincent Bousquet <vincent.bousqt@gmail.com>
2021-03-06 00:42:28 +0100

    Add support for situation with PinMame + FlexDMD both using dmdevice.dll

--- FlexDMD/DMDDevice.cs
+++ FlexDMD/DMDDevice.cs
@@ -47,6 +47,21 @@ namespace FlexDMD
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         delegate void GameSettingsDelegate(string gameName, ulong hardwareGeneration, IntPtr options);
 
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int CreateDeviceDelegate();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate int OpenCloseDeviceDelegate(int id);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate void RenderDeviceDelegate(int id, ushort width, ushort height, IntPtr currbuffer);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate void RenderAlphaNumericDeviceDelegate(int id, NumericalLayout numericalLayout, IntPtr seg_data, IntPtr seg_data2);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        delegate void GameSettingsDeviceDelegate(int id, string gameName, ulong hardwareGeneration, IntPtr options);
+
         private IntPtr _dllhandle = IntPtr.Zero;
         private readonly OpenCloseDelegate _open = null;
         private readonly OpenCloseDelegate _close = null;
@@ -55,6 +70,15 @@ namespace FlexDMD
         private readonly RenderDelegate _renderGray2 = null;
         private readonly RenderAlphaNumericDelegate _renderAlphaNumeric = null;
         private readonly GameSettingsDelegate _gameSettings = null;
+        private readonly CreateDeviceDelegate _createDevice = null;
+        private readonly OpenCloseDeviceDelegate _openDevice = null;
+        private readonly OpenCloseDeviceDelegate _closeDevice = null;
+        private readonly RenderDeviceDelegate _renderRgb24Device = null;
+        private readonly RenderDeviceDelegate _renderGray4Device = null;
+        private readonly RenderDeviceDelegate _renderGray2Device = null;
+        private readonly RenderAlphaNumericDeviceDelegate _renderAlphaNumericDevice = null;
+        private readonly GameSettingsDeviceDelegate _gameSettingsDevice = null;
+        private int _id = -1;
 
         public DMDDevice()
         {
@@ -75,10 +99,26 @@ namespace FlexDMD
                 if (renderGray4Handle != IntPtr.Zero) _renderGray4 = (RenderDelegate)Marshal.GetDelegateForFunctionPointer(renderGray4Handle, typeof(RenderDelegate));
                 var renderRgb24Handle = NativeLibrary.GetProcAddress(_dllhandle, "Render_RGB24");
                 if (renderRgb24Handle != IntPtr.Zero) _renderRgb24 = (RenderDelegate)Marshal.GetDelegateForFunctionPointer(renderRgb24Handle, typeof(RenderDelegate));
-                var renderAlphaNumeric = NativeLibrary.GetProcAddress(_dllhandle, "Render_PM_Alphanumeric_Frame");
-                if (renderAlphaNumeric != IntPtr.Zero) _renderAlphaNumeric = (RenderAlphaNumericDelegate)Marshal.GetDelegateForFunctionPointer(renderAlphaNumeric, typeof(RenderAlphaNumericDelegate));
+                var renderAlphaNumericHandle = NativeLibrary.GetProcAddress(_dllhandle, "Render_PM_Alphanumeric_Frame");
+                if (renderAlphaNumericHandle != IntPtr.Zero) _renderAlphaNumeric = (RenderAlphaNumericDelegate)Marshal.GetDelegateForFunctionPointer(renderAlphaNumericHandle, typeof(RenderAlphaNumericDelegate));
                 var gameSettingsHandle = NativeLibrary.GetProcAddress(_dllhandle, "PM_GameSettings");
                 if (gameSettingsHandle != IntPtr.Zero) _gameSettings = (GameSettingsDelegate)Marshal.GetDelegateForFunctionPointer(gameSettingsHandle, typeof(GameSettingsDelegate));
+                var createDeviceHandle = NativeLibrary.GetProcAddress(_dllhandle, "Create_Device");
+                if (createDeviceHandle != IntPtr.Zero) _createDevice = (CreateDeviceDelegate)Marshal.GetDelegateForFunctionPointer(createDeviceHandle, typeof(CreateDeviceDelegate));
+                var openDeviceHandle = NativeLibrary.GetProcAddress(_dllhandle, "Open_Device");
+                if (openDeviceHandle != IntPtr.Zero) _openDevice = (OpenCloseDeviceDelegate)Marshal.GetDelegateForFunctionPointer(openDeviceHandle, typeof(OpenCloseDeviceDelegate));
+                var closeDeviceHandle = NativeLibrary.GetProcAddress(_dllhandle, "Close_Device");
+                if (closeDeviceHandle != IntPtr.Zero) _closeDevice = (OpenCloseDeviceDelegate)Marshal.GetDelegateForFunctionPointer(closeDeviceHandle, typeof(OpenCloseDeviceDelegate));
+                var renderGray2DeviceHandle = NativeLibrary.GetProcAddress(_dllhandle, "Render_4_Shades_Device");
+                if (renderGray2DeviceHandle != IntPtr.Zero) _renderGray2Device = (RenderDeviceDelegate)Marshal.GetDelegateForFunctionPointer(renderGray2DeviceHandle, typeof(RenderDeviceDelegate));
+                var renderGray4DeviceHandle = NativeLibrary.GetProcAddress(_dllhandle, "Render_16_Shades_Device");
+                if (renderGray4DeviceHandle != IntPtr.Zero) _renderGray4Device = (RenderDeviceDelegate)Marshal.GetDelegateForFunctionPointer(renderGray4DeviceHandle, typeof(RenderDeviceDelegate));
+                var renderRgb24DeviceHandle = NativeLibrary.GetProcAddress(_dllhandle, "Render_RGB24_Device");
+                if (renderRgb24DeviceHandle != IntPtr.Zero) _renderRgb24Device = (RenderDeviceDelegate)Marshal.GetDelegateForFunctionPointer(renderRgb24DeviceHandle, typeof(RenderDeviceDelegate));
+                var renderAlphaNumericDeviceHandle = NativeLibrary.GetProcAddress(_dllhandle, "Render_PM_Alphanumeric_Frame_Device");
+                if (renderAlphaNumericDeviceHandle != IntPtr.Zero) _renderAlphaNumericDevice = (RenderAlphaNumericDeviceDelegate)Marshal.GetDelegateForFunctionPointer(renderAlphaNumericDeviceHandle, typeof(RenderAlphaNumericDeviceDelegate));
+                var gameSettingsDeviceHandle = NativeLibrary.GetProcAddress(_dllhandle, "PM_GameSettings_Device");
+                if (gameSettingsDeviceHandle != IntPtr.Zero) _gameSettingsDevice = (GameSettingsDeviceDelegate)Marshal.GetDelegateForFunctionPointer(gameSettingsDeviceHandle, typeof(GameSettingsDeviceDelegate));
             }
             else
             {
@@ -105,45 +145,48 @@ namespace FlexDMD
 
         public int Open()
         {
+            if (_createDevice != null && _openDevice != null)
+            {
+                _id = _createDevice();
+                return _openDevice(_id);
+            }
             if (_open != null) return _open();
             return 0;
         }
 
         public int Close()
         {
+            if (_id >= 0 && _closeDevice != null) return _closeDevice(_id);
             if (_close != null) return _close();
             return 0;
         }
 
         public void RenderRgb24(ushort width, ushort height, IntPtr currbuffer)
         {
-            _renderRgb24?.Invoke(width, height, currbuffer);
+            if (_id >= 0) _renderRgb24Device?.Invoke(_id, width, height, currbuffer); else _renderRgb24?.Invoke(width, height, currbuffer);
         }
 
         public void RenderGray4(ushort width, ushort height, IntPtr currbuffer)
         {
-            _renderGray4?.Invoke(width, height, currbuffer);
+            if (_id >= 0) _renderGray4Device?.Invoke(_id, width, height, currbuffer); else _renderGray4?.Invoke(width, height, currbuffer);
         }
 
         public void RenderGray2(ushort width, ushort height, IntPtr currbuffer)
         {
-            _renderGray2?.Invoke(width, height, currbuffer);
+            if (_id >= 0) _renderGray2Device?.Invoke(_id, width, height, currbuffer); else _renderGray2?.Invoke(width, height, currbuffer);
         }
 
         public void RenderlphaNumeric(NumericalLayout numericalLayout, IntPtr seg_data, IntPtr seg_data2)
         {
-            _renderAlphaNumeric?.Invoke(numericalLayout, seg_data, seg_data2);
+            if (_id >= 0) _renderAlphaNumericDevice?.Invoke(_id, numericalLayout, seg_data, seg_data2); else _renderAlphaNumeric?.Invoke(numericalLayout, seg_data, seg_data2);
         }
 
         public void GameSettings(string gameName, ulong hardwareGeneration, PMoptions options)
         {
-            if (_gameSettings != null)
-            {
-                IntPtr ptr = Marshal.AllocHGlobal(19 * sizeof(int));
-                Marshal.StructureToPtr(options, ptr, true);
-                _gameSettings(gameName, hardwareGeneration, ptr);
-                Marshal.FreeHGlobal(ptr);
-            }
+            IntPtr ptr = Marshal.AllocHGlobal(19 * sizeof(int));
+            Marshal.StructureToPtr(options, ptr, true);
+            if (_id >= 0) _gameSettingsDevice?.Invoke(_id, gameName, hardwareGeneration, ptr); else _gameSettings?.Invoke(gameName, hardwareGeneration, ptr);
+            Marshal.FreeHGlobal(ptr);
         }
 
         /*
