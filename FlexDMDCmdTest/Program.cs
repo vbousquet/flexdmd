9597bdaefc2a36b56b19f55a97de348960f73fa7
Vincent Bousquet <vincent.bousqt@gmail.com>
2021-04-16 23:58:48 +0200

    Update to Test app

--- FlexDMDCmdTest/Program.cs
+++ FlexDMDCmdTest/Program.cs
@@ -21,9 +21,11 @@ namespace FlexDMDTest
             // udmd.DisplayScene00("Metal Slug.UltraDMD/Explosion.gif", "SCENE", 0, "2", 0, 0, 1000, 0);
             // udmd.DisplayScene00("dmds/bally.avi", "SCENE", 0, "2", 0, 0, 1000, 0);
             // udmd.DisplayScoreboard(4, 0, 1000, 2000, 3000, 4000, "LEFT", "RIGHT");
-            // dmd.DisplayScene00("Metal Slug.UltraDMD/IntroMS.wmv", "SCENE", 0, "2", 0, 0, 5000, 0);
-            // dmd.DisplayScene00Ex("Diablo.UltraDMD/act1.wmv", "SCENE", 5, 15, "Test n 2", 5, 15, 14, 5000, 14);
-            udmd.DisplayScene00("Futurama.UltraDMD/free-play.mp4", "Color Test", 15, "Scene Test", 15, 14, 1000, 14);
+            // udmd.DisplayScene00("Metal Slug.UltraDMD/IntroMS.wmv", "SCENE", 0, "2", 0, 0, 5000, 0);
+            // udmd.DisplayScene00Ex("Diablo.UltraDMD/act1.wmv", "SCENE", 5, 15, "Test n 2", 5, 15, 14, 5000, 14);
+            // udmd.DisplayScene00("Futurama.UltraDMD/free-play.mp4", "Color Test", 15, "Scene Test", 15, 14, 1000, 14);
+            udmd.DisplayScene00("Countdown2.mp4", "Color Test", 15, "Scene Test", 15, 14, 5000, 14);
+            // udmd.DisplayScene00("big_buck_bunny.mp4", "Color Test", 15, "Scene Test", 15, 14, 1000, 14);
             // udmd.DisplayScene00("Diablo.UltraDMD/black.jpg&add", "Color Test", 15, "Scene Test", 15, 14, 1000, 14);
             // udmd.DisplayScene01("", "Diablo.UltraDMD/black.jpg&add", "Flex DMD Test", 5, 15, 0, 5000, 0);
             // udmd.DisplayScene00Ex("FlexDMD.Resources.dmds.williams.gif", "", 5, 15, "", 0, 0, 14, 10000, 14);
