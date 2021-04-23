8582b21aa3bb5e89c8d23fa0f0d97e6bc0ef63e6
Vincent Bousquet <vincent.bousqt@gmail.com>
2021-04-23 22:23:51 +0200

    Minor clean up

--- FlexDMD/Actors/Frame.cs
+++ FlexDMD/Actors/Frame.cs
@@ -7,8 +7,8 @@ namespace FlexDMD
     // [Guid("3EF157CD-9A77-4B70-995F-1F3FAFCA4412"), ComVisible(true), ClassInterface(ClassInterfaceType.None)]
     public class Frame : Actor, IFrameActor
     {
-        private SolidBrush _borderBrush = new SolidBrush(Color.White);
-        private SolidBrush _fillBrush = new SolidBrush(Color.Black);
+        private readonly SolidBrush _borderBrush = new SolidBrush(Color.White);
+        private readonly SolidBrush _fillBrush = new SolidBrush(Color.Black);
 
         public int Thickness { get; set; } = 2;
         public Color BorderColor { get; set; } = Color.White;
@@ -21,15 +21,15 @@ namespace FlexDMD
             if (Fill)
             {
                 _fillBrush.Color = FillColor;
-                graphics.FillRectangle(_fillBrush, new RectangleF(X + Thickness, Y + Thickness, Width - 2 * Thickness, Height - 2 * Thickness));
+                graphics.FillRectangle(_fillBrush, X + Thickness, Y + Thickness, Width - 2 * Thickness, Height - 2 * Thickness);
             }
             if (Thickness > 0)
             {
                 _borderBrush.Color = BorderColor;
-                graphics.FillRectangle(_borderBrush, new RectangleF(X, Y, Width, Thickness));
-                graphics.FillRectangle(_borderBrush, new RectangleF(X, Y + Height - Thickness, Width, Thickness));
-                graphics.FillRectangle(_borderBrush, new RectangleF(X, Y + Thickness, Thickness, Height - 2 * Thickness));
-                graphics.FillRectangle(_borderBrush, new RectangleF(X + Width - Thickness, Y + Thickness, Thickness, Height - 2 * Thickness));
+                graphics.FillRectangle(_borderBrush, X, Y, Width, Thickness);
+                graphics.FillRectangle(_borderBrush, X, Y + Height - Thickness, Width, Thickness);
+                graphics.FillRectangle(_borderBrush, X, Y + Thickness, Thickness, Height - 2 * Thickness);
+                graphics.FillRectangle(_borderBrush, X + Width - Thickness, Y + Thickness, Thickness, Height - 2 * Thickness);
             }
         }
     }
